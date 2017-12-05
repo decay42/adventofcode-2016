3196a5fd6c00e57bcb1c6437e355cf9821b7f0e5
Jan Pfeiffer <liberry@ymail.com>
2017-12-05 19:03:51 +0100

    changed password storage from string to array

--- src/day5/2.js
+++ src/day5/2.js
@@ -3,28 +3,23 @@ import md5 from 'md5'
 const input = 'wtnhxymk'
 
 let i = 0
-let password = '________'
+let password = new Array(8)
+for (let i = 0; i < password.length; i++) {
+  password[i] = '_'
+}
 
-while (true) {
-  if (password.split('').every(c => c !== '_')) {
-    break
-  }
-
+while (password.some(c => c === '_')) {
   const currentHash = md5(input + i++)
 
   if (currentHash.startsWith('00000')) {
     const index = +currentHash[5]
     const replacement = currentHash[6]
-    if (/[0-7]/.test(index) && password[+index] === '_') {
+    if (/[0-7]/.test(index) && password[index] === '_') {
       console.log(index, replacement)
-      password = replaceCharAt(password, index, replacement)
+      password[index] = replacement
       console.log(password)
     }
   }
 }
 
-console.log(password)
-
-function replaceCharAt (string, index, replacement) {
-  return (string.substr(0, index) + replacement + string.substr(index + 1))
-}
+console.log(password.join(''))
